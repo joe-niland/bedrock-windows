4a44da381f6b4e36eba217c670c244ce2170fe51
Joe Niland <joe@originalmind.com.au>
2014-10-15 12:26:26 +1100

    handling multiple sites per VM

--- README.md
+++ README.md
@@ -9,19 +9,28 @@ about
 This PowerShell script makes use of [Chocolatey](http://chocolatey.org) to provision your Windows machine with everything you need to create a new bedrock instance. It's been tested on Windows 7 and 8.1.
 It is designed to be idempotent so you can run it as many times as you want.
 
-Requirements
+**The script needs some clean-up but it works!**
+
+requirements
 ------------
 Having a working git executable in your PATH is a requirement. If you don't have this, I recommend downloading [GitHub for Windows](https://windows.github.com/), and opening the "Git Shell" shortcut that it installs.
 
-Dependencies
+dependencies
 --------------
 The script installs [Chocolatey](http://chocolatey.org) and uses it to install the Windows versions of the required prerequisites:
 * vagrant
 * virtualbox
-    
-It then pulls git projects from [bedrock](https://github.com/roots/bedrock)and [bedrock-ansible](https://github.com/roots/bedrock-ansible). It also splices in a [gist](https://gist.github.com/starise/e90d981b5f9e1e39f632) from [Andrea Brandi](https://github.com/starise).
 
-Why did I create this?
+It also uses the PowerShell module manager to import a required module:
+* [PsGet](http://psget.net/)
+    
+It then pulls git projects from:
+* [bedrock](https://github.com/roots/bedrock), and 
+* [bedrock-ansible](https://github.com/roots/bedrock-ansible). 
+
+.. and splices in a [gist](https://gist.github.com/starise/e90d981b5f9e1e39f632) from [Andrea Brandi](https://github.com/starise).
+
+why did I create this?
 --------------------
 Whenever I have to set up a new open-source framework/tool (particularly on Windows), it invariably involves piecing together instructions from readme's/gists on github and/or peoples' blogs. I feel that this type of thing should live in a script! This is so:
 * it can be reused - only a few people need to go through the pain
@@ -32,6 +41,19 @@ BTW, if you're more of a yeoman type of Sir, head over to: https://github.com/pa
 
 usage
 =====
+
+notes
+-------
+
+The instructions below assume the following:
++ you organise your wordpress themes under a common directory, say `c:\dev\wp-themes`
++ you'll maintain a single `bedrock-ansible` directory under this common directory. Within this `bedrock-ansible` directory you'll maintain one **or more** local developments sites. This essentially maps to one Vagrant-managed VM. 
+
+    Remember that Ansible's top level playbook (`site.yml`) file defines your whole infrastructure, so you can define multiple sites and/or hosts.
+
+steps to generate your VM
+--------------------------
+
 1. git clone https://github.com/joe-niland/bedrock-windows.git
 2. Copy provision-bedrock.ps1 to your wordpress development root, e.g. c:\dev\wp themes\
 
@@ -57,6 +79,13 @@ usage
    See https://github.com/roots/bedrock-ansible#usage for details.
 7. Then, navigate to _your_dev_root\project_name\bedrock-ansible_ and type `vagrant up`
 8. Open your browser and go to 'project_name.dev/' to view your freshly created Bedrock site!
+
+generating your Nth projects
+-----------------------------
+
+As mentioned above there's no need to maintain multiple bedrock-ansible instances (and therefore multiple VMs) unless you really need to seperate them.
+
+You can create as many subsequent projects as you like using this script. It will not touch the `bedrock-ansible` directory if it already exists.
 
 contributions
 =============
--- provision-bedrock.ps1
+++ provision-bedrock.ps1
@@ -1,9 +1,19 @@
 <#
  # Create a fresh Wordpress bedrock VM using bedrock-ansible and vagrant. 
  #>
-$DebugPreference = "SilentlyContinue" # Change to "Continue" in Dev
+$DebugPreference = "Continue" # Change to "Continue" in Dev
 
-Set-Variable -Name $aptMirror -Value ""
+Set-Variable -Name aptMirror -Value ""
+Set-Variable -Name vagrantConfigIncludeSitesFile -Value "site_config = YAML::load_file(`"../.sites/sites.yml`")"
+Set-Variable -Name vagrantConfigSyncedFolders -Value @"
+  site_config['sites'].each do |site_name|
+    config.vm.synced_folder '../' + site_name, '/srv/www/' + site_name + '/current',
+        id: 'current',
+        owner: 'vagrant',
+        group: 'www-data',
+        mount_options: ['dmode=776', 'fmode=775']
+  end
+"@
 
 function Download-File {
    param ( [string]$url, [string]$file )
@@ -12,6 +22,19 @@ function Download-File {
    $downloader = new-object System.Net.WebClient
    $downloader.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;
    $downloader.DownloadFile($url, $file)
+}
+
+function Install-PsGet {
+
+  $psget = get-module -listavailable | where-object { $_.name -eq "PsGet" }
+  if ($psget -eq $null) {
+      Write-Host "Installing PsGet"
+      (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+      
+  } else {
+      Write-Host "PsGet already installed."
+  }
+  
 }
 
 function Update-VagrantPlugin {
@@ -47,6 +70,54 @@ function Update-VagrantPlugin {
     }
 }
 
+function Create-SitesFile {
+   <#
+        .DESCRIPTION
+        Creates sites yaml file.
+        
+         .PARAMETER installDir
+        Site local directory path.
+
+  #>
+  param ( [string]$installDir )
+
+  $sitesDir = ".\.sites\"
+  $sitesFile = ".\.sites\sites.yml"
+
+  $siteName = (gi $installDir).Name
+
+  if (!(Test-Path $sitesDir)) {
+    New-Item -Type Directory -Name $sitesDir | Out-Null
+  }
+
+  if (!(Test-Path $sitesFile)) {
+    Write-Host "Creating sites file: $sitesFile"
+    New-Item -Type File -Name $sitesFile | Out-Null
+    Add-Content $sitesFile "sites:"
+  }
+
+  $sitesFile = (Resolve-Path $sitesFile)
+
+  import-module PowerYaml
+  $sites = Get-Yaml -FromFile $sitesFile
+
+  if ($sites.Values -notcontains $siteName) {
+    Write-Host "Adding site to sites file: $sitesFile"
+    Add-Content $sitesFile "`n  - ${siteName}"
+  } else {
+    Write-Host "$siteName already added to sites file"
+  }
+
+  # Remove any blank lines
+  (gc $sitesFile) | ? {$_.trim() -ne "" } | sc $sitesFile
+}
+
+
+
+
+
+############################################################################################################
+
 Write-Host Run this from your dev root directory, e.g. c:\users\me\documents\dev\
 Write-Host Then pass in the project directory as a param.
 
@@ -60,11 +131,14 @@ if ($projectDir -eq "" -or $projectDir -eq $null) {
   exit
 }
 
+$hostName = "$projectDir.dev"
+
 $installDir = Join-Path (Get-Location) $projectDir
 Write-Host Installing to $installDir
 
 if (Test-Path -Path $installDir) { 
-  Write-Warning "$installDir already exists"
+  Write-Warning "$installDir already exists. Exiting"
+  exit
 }
 
 
@@ -75,32 +149,28 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit
 }
 
-
-$choice = ""
-while ($choice -notmatch "[y|n]") {
-  $choice = read-host "Do you want to change the apt source to a mirror (https://launchpad.net/ubuntu/+archivemirrors) ? (y/N)"
-}
-
-if ($choice -eq "y") {
-    $aptMirror = read-host "Enter the mirror server's host name, e.g. au.archive.ubuntu.com (blank will use the default)"
-}
-
-$choice = ""
-while ($choice -notmatch "[y|n]"){
-  $choice = read-host "Do you want to continue? (y/N)"
-}
+# Continue?
+$choice = read-host "Do you want to continue? (y/N)"
 
 if ($choice -ne "y") {
   Write-Host "Quitting. Nothing was done..."
   exit
 }
 
+
+
+##### Get prerequisites
+
+Install-PsGet
+import-module PsGet
+install-module PowerYaml
+
 # Detect OS
 $Win7 = [Environment]::OSVersion.Version -eq (new-object 'Version' 6,1)
 $Win8 = [Environment]::OSVersion.Version -eq (new-object 'Version' 6,2)
 $Win81 = [Environment]::OSVersion.Version -eq (new-object 'Version' 6,3)
 
-## host machine
+
 # Install chocolatey <-- https://chocolatey.org/
 # todo: better way to detect chocolatey
 if (!(Test-Path -Path "$Env:systemdrive\programdata\chocolatey\bin")) {
@@ -164,9 +234,28 @@ if ($Win8) {
   }
 }
 
-
 # Download bedrock-ansible and bedrock
 if (!(Test-Path "bedrock-ansible")) {
+
+  # Set hostname
+  $choice = read-host "Please specify the Virtual Machine's host name (blank to use default: $hostName)"
+
+  if ($choice -ne "") {
+      $hostName = $choice
+  }
+
+  # Set apt mirror
+  $choice = ""
+  while ($choice -notmatch "[y|n]") {
+    $choice = read-host "Do you want to change the apt source to a mirror (https://launchpad.net/ubuntu/+archivemirrors) ? (y/N)"
+  }
+
+  if ($choice -eq "y") {
+      $aptMirror = read-host "Enter the mirror server's host name, e.g. au.archive.ubuntu.com (blank to use Ubuntu's default)"
+  }
+
+  $hostName = $hostName.Replace("_", "-"); # todo: clean host name properly
+
   Write-Host "Cloning bedrock-ansible"
   git clone https://github.com/roots/bedrock-ansible.git
 
@@ -178,13 +267,9 @@ if (!(Test-Path "bedrock-ansible")) {
   Move-Item $vagrantFilepath ($vagrantFilepath + ".original")
 
   # Get the Windows modifications
-  Download-File https://gist.githubusercontent.com/starise/e90d981b5f9e1e39f632/raw/d96002240cf82c60538652d8fcbffea46f256303/Vagrantfile $vagrantFilepath
+  # Download-File https://gist.githubusercontent.com/starise/e90d981b5f9e1e39f632/raw/d96002240cf82c60538652d8fcbffea46f256303/Vagrantfile $vagrantFilepath
+  Download-File https://raw.githubusercontent.com/joe-niland/bedrock-windows/master/templates/Vagrantfile.template $vagrantFilepath
   Download-File https://gist.githubusercontent.com/starise/e90d981b5f9e1e39f632/raw/778325547427aec53bdc38aed217b311f0cb68f4/windows.sh ($bedrockAnsiblePath + "\windows.sh")
-
-  # Set up Vagrantfile for our environment
-  Write-Host "Editing bedrock-ansible\Vagrantfile and set: hostname and paths to web root"
-  $hostName = "$projectDir.dev".Replace("_", "-"); # todo: clean host name properly
-  Write-Host "Host name: $hostName"
 
   $vagrantSetAptMirror = ""
   if ($aptMirror -ne "") {
@@ -193,17 +278,25 @@ if (!(Test-Path "bedrock-ansible")) {
     (gc $vagrantFilepath -raw) | % { $_ -replace '(?s)(?<provider>config.vm.provider.+?end)', ('${provider}' + "`n`n$vagrantSetAptMirror") } | sc $vagrantFilepath
   }
 
-  # Replace values
+  # Set up Vagrantfile for our environment
+  Write-Host "Editing bedrock-ansible\Vagrantfile and set: hostname and paths to web root"
+  Write-Host "Using host name: $hostName"
+
+  # Modify vagrantfile to handle multiple sites
+  # (gc $vagrantFilepath -raw) | % { $_ -replace '(?s)(?<syncfolder>config.vm.synced_folder.+?\])', ("$vagrantConfigIncludeSitesFile`n$vagrantConfigSyncedFolders") } | sc $vagrantFilepath
+
+  # Modify selected lines in the Vagrantfile
   (gc $vagrantFilepath) | ForEach-Object {
     Write-Debug $_
     if ($_.ToString() -like "*config.vm.hostname*") {
         $_.replace('example.dev', $hostName)
     } elseif ($_.ToString() -like "*config.vm.synced_folder*") {
-        $_.replace('../example.dev', "../$projectDir").replace('/srv/www/example.dev', "/srv/www/$hostName") # We're using the hostName internally because the Ansible playbook expects it.
+        # $_.replace('../example.dev', "../$projectDir").replace('/srv/www/example.dev', "/srv/www/$hostName") # We're using the hostName internally because the Ansible playbook expects it.
+        "$vagrantConfigIncludeSitesFile`n$vagrantConfigSyncedFolders"
     } else {
         $_
     }
-    Write-Debug $_
+    # Write-Debug $_
   } | sc $vagrantFilepath
   
   Write-Host "Editing bedrock-ansible\group_vars\development and set site-specific options"
@@ -213,15 +306,18 @@ if (!(Test-Path "bedrock-ansible")) {
   (gc $groupVarsDevPath).replace("example.dev", $hostName) | sc $groupVarsDevPath
 }
 
-# Check if the 
+
+
+# Check if the project directory already exists
 if (!(Test-Path "$installDir")) {
   # Create location for bedrock project
   New-Item -Type Directory -Path $installDir | Out-Null
 
   Write-Host "Cloning bedrock into $installDir"
   git clone https://github.com/roots/bedrock.git $installDir
-}
 
+  Create-SitesFile $installDir
+}
 
 ### Vagrant plugin setup
 Update-VagrantPlugin "hostsupdater" $true
